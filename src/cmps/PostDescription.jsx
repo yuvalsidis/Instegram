0be9abab865e7b3d76194b01ba863e79b41c366b
yuvalsidis <yuvalsidis@gmail.com>
2024-04-06 17:18:01 +0300

    Add translation to PostDesctiption with button

--- src/cmps/PostDescription.jsx
+++ src/cmps/PostDescription.jsx
@@ -1,27 +1,47 @@
-import { useState } from "react"
-import { utilService } from "../services/util.service";
+import React, { useState, useEffect } from "react";
 
 export function PostDescription({ post }) {
     const [expanded, setExpanded] = useState(false)
-    const maxChars = utilService.getRandomIntInclusive(70, 100)
+    const [translatedText, setTranslatedText] = useState('')
+    const [isTranslatedToHebrew, setIsTranslatedToHebrew] = useState(false)
+
+    useEffect(() => {
+        async function translateText() {
+            try {
+                const targetLanguage = isTranslatedToHebrew ? 'iw' : 'en'
+                const response = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(post.txt)}`);
+                const data = await response.json();
+                const translatedText = data[0][0][0]; 
+                setTranslatedText(translatedText);
+            } catch (error) {
+                console.error('Translation error:', error)
+            }
+        }
+
+        translateText();
+    }, [post.txt, isTranslatedToHebrew]);
 
     const toggleExpanded = () => {
         setExpanded(!expanded);
     };
 
+    const toggleTranslation = () => {
+        setIsTranslatedToHebrew(!isTranslatedToHebrew)
+    };
+
     return (
         <div className="post-description">
             <p>
-                <span className="post-description-fullname">{post.by.fullname} </span>
-                {expanded ? post.txt : `${post.txt.slice(0, maxChars)}${post.txt.length > maxChars ? '...' : ''}`}
-                <span> {!expanded && post.txt.length > maxChars && (
+                {expanded ? translatedText : `${translatedText.slice(0, 100)}${translatedText.length > 100 ? '...' : ''}`}
+                <span> {!expanded && translatedText.length > 100 && (
                     <button className="more-btn" onClick={toggleExpanded}>
                         More
                     </button>
                 )}</span>
             </p>
-                <button className="translate-btn">See translation</button>
+            <button className="translate-btn" onClick={toggleTranslation}>
+                {isTranslatedToHebrew ? "See orginal" : "See translation"}
+            </button>
         </div>
-
-    )
+    );
 }
