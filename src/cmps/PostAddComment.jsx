bd16a3213631fdf5576aae5fd721e352cb36fc9a
yuvalsidis <yuvalsidis@gmail.com>
2024-04-06 20:17:38 +0300

    Adjust change on PostAddComment textarea

--- src/cmps/PostAddComment.jsx
+++ src/cmps/PostAddComment.jsx
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import SentimentSatisfiedOutlinedIcon from '@mui/icons-material/SentimentSatisfiedOutlined';
 
 export function PostAddComment({ post }) {
+    const [text, setText] = useState('');
+
+    const handleChange = (event) => {
+        const textarea = event.target;
+        textarea.style.height = 'auto'; // Reset height to auto
+        const newHeight = `${textarea.scrollHeight}px`;
+        textarea.style.height = newHeight;
+
+        // Check if the scroll height decreased
+        if (textarea.scrollHeight < textarea.offsetHeight) {
+            textarea.style.height = `${textarea.scrollHeight}px`;
+        }
+
+        setText(textarea.value);
+    };
 
     return (
-        <dev className="post-add-comment">
-            <textarea className="post-add-comment-textarea"
-                placeholder="Add a comment"
-            >
-            </textarea>
+        <div className="post-add-comment">
+            <textarea
+                className="post-add-comment-textarea"
+                value={text}
+                onChange={handleChange}
+                placeholder="Add a comment..."
+            />
             <button className="post-comment-btn">Post</button>
-
-            <SentimentSatisfiedOutlinedIcon className='emoji-icon'/>
-
-
-        </dev>
-    )
+            <SentimentSatisfiedOutlinedIcon className="emoji-icon" />
+        </div>
+    );
 }
