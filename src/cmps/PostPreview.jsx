b2624ea89bf0f54961f77dddf24183b38834ba23
yuvalsidis <yuvalsidis@gmail.com>
2024-04-07 00:58:07 +0300

    Adjust postpreviewstats transformtranslate

--- src/cmps/PostPreview.jsx
+++ src/cmps/PostPreview.jsx
@@ -5,11 +5,13 @@ export function PostPreview({ post }) {
         <>
             <img src={post.imgUrl}></img>
             <div className="post-preview-stats">
-                <div>
+                <div className="post-preview-stats-likes">
                     <img className='icon' src="../../public/icons/WhiteLike.svg" alt="WhiteLike Icon" />
+                    <p>{post.likedBy.length}</p>
                 </div>
                 <div>
-                    <img className='icon' src="../../public/icons/WhiteComment.svg" alt="WhiteComment Icon" />
+                    <img className='post-preview-stats-comments' src="../../public/icons/WhiteComment.svg" alt="WhiteComment Icon" />
+                    <p>{post.comments.length}</p>
                 </div>
             </div>
         </>
