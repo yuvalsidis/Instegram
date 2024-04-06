f542c1c283f89dfdb4bf392a26fa0fcb3e57d35e
yuvalsidis <yuvalsidis@gmail.com>
2024-04-07 00:48:37 +0300

    Edit svgs to make some white by fill and border

--- src/cmps/PostPreview.jsx
+++ src/cmps/PostPreview.jsx
@@ -1,9 +1,17 @@
 
 
-export function PostPreview({post}){
+export function PostPreview({ post }) {
     return (
         <>
-             <img src={post.imgUrl}></img>
+            <img src={post.imgUrl}></img>
+            <div className="post-preview-stats">
+                <div>
+                    <img className='icon' src="../../public/icons/WhiteLike.svg" alt="WhiteLike Icon" />
+                </div>
+                <div>
+                    <img className='icon' src="../../public/icons/WhiteComment.svg" alt="WhiteComment Icon" />
+                </div>
+            </div>
         </>
     )
 }
