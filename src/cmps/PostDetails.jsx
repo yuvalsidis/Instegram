664d63012f29bb77bc9e1b7427902783f86bc8bf
yuvalsidis <yuvalsidis@gmail.com>
2024-04-08 21:46:16 +0300

    Linking and add all six Post components to PostDetails

--- src/cmps/PostDetails.jsx
+++ src/cmps/PostDetails.jsx
@@ -1,12 +1,25 @@
+import { PostAddComment } from "./PostAddComment"
+import { PostHeader } from "./PostHeader"
+import { PostDescription } from "./PostDescription"
+import { PostPhotos } from "./PostPhotos"
+import { PostActions } from "./PostActions"
+import { PostComments } from "./PostComments"
+import { PostLikes } from "./PostLikes"
 
-export function PostDetails({ post }) {
-
-    console.log('post is ', post)
-    
+export function PostDetails({ post, onUpdatePost}) {
+    console.log('Post', post)
     return (
         <div className="post-details">
             <div>
-
+                <PostPhotos post={post} />
+            </div>
+            <div>
+                <PostHeader post={post} />
+                <PostDescription post={post} />
+                <PostComments post={post} />
+                <PostActions post={post} />
+                <PostLikes post={post} />
+                <PostAddComment post={post} onUpdatePost={onUpdatePost} />
             </div>
         </div>
     )
--- src/pages/PostDetailsPage.jsx
+++ src/pages/PostDetailsPage.jsx
@@ -1,5 +1,5 @@
 
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 import { PostDetails } from "../cmps/PostDetails"
 import { postService } from "../services/post.service.local"
 import { useState, useEffect } from "react"
@@ -7,19 +7,19 @@ import { LOADING_DONE, LOADING_START } from "../store/system.reducer"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
 import { useSelector } from "react-redux"
 import { store } from "../store/store"
+import { removePost, addPost, updatePost, loadPosts } from "../store/post.actions"
 
-
-export function PostDetailsPage(){
-    const [post,SetPost] = useState('null')
+export function PostDetailsPage() {
+    const [post, SetPost] = useState(null)
     const isLoading = useSelector(storeState => storeState.systemModule.isLoading)
+    const { postId } = useParams()
 
-    const {postId} = useParams()
-
-    useEffect(()=>{
+    useEffect(() => {
         onGetPost()
     },[])
 
     function onGetPost() {
+        console.log('Trying get post')
         store.dispatch({ type: LOADING_START, })
         postService.getById(postId)
             .then((matchedPost) => {
@@ -34,11 +34,23 @@ export function PostDetailsPage(){
 
     }
 
-   
-    if(isLoading) return <div>Loading</div>
-    return(
+    function onUpdatePost(post) {
+        updatePost(post)
+            .then(() => {
+                showSuccessMsg('Post updated successfully')
+            })
+            .catch((err) => {
+                showErrorMsg('Error occured when updating posts', err)
+            })
+    }
+
+
+    console.log('post in page', post)
+
+    if (isLoading) return <div>Loading</div>
+    return (
         <section className="post-details-page">
-            <PostDetails post={post}/>
+            <PostDetails post={post} onUpdatePost={onUpdatePost} />
         </section>
     )
 }
