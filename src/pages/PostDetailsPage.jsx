06e30f896e29b4f8e51ed6812ec198fa9e7cfa77
yuvalsidis <yuvalsidis@gmail.com>
2024-04-08 18:49:35 +0300

    Add post details and link to PostDetailsPage

--- src/pages/PostDetailsPage.jsx
+++ src/pages/PostDetailsPage.jsx
@@ -1,9 +1,16 @@
 
+import {useParams} from "react-router-dom"
+import { PostDetails } from "../cmps/PostDetails"
+
 export function PostDetailsPage(){
     console.log('arrived to destination post details!')
+    const {postId} = useParams()
+    console.log('post id is :', postId)
+
     return(
         <section className="post-details-page">
             <h1>Post Details Page</h1>
+            <PostDetails/>
         </section>
     )
 }
