2906d5adf843a86426723084aa735cfd47c54a9c
yuvalsidis <yuvalsidis@gmail.com>
2024-04-08 22:58:30 +0300

    Add useLocation to determine if p page exist in postComments rendering

--- src/cmps/PostComments.jsx
+++ src/cmps/PostComments.jsx
@@ -1,10 +1,15 @@
+import { useLocation } from "react-router-dom"
 
 
-export function PostComments({post}){
+export function PostComments({ post }) {
+  const location = useLocation()
+  const isPostDetailsPage = location.pathname.includes('/p/')
 
-    return (
-        <div className="post-comments">
-          <button className="view-comments-btn">View all {post.comments.length} comments</button>
-        </div>
-    )
+  console.log('location /p/??',isPostDetailsPage)
+
+  return (
+    <div className="post-comments">
+      <button className="view-comments-btn">View all {post.comments.length} comments</button>
+    </div>
+  )
 }
