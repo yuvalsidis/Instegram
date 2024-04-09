b9e0ab162b8a8ff07b6a77b416f3ce80517d5d0e
yuvalsidis <yuvalsidis@gmail.com>
2024-04-09 19:59:28 +0300

    Add condition rendering by checking if isPostDetailsPage

--- src/cmps/PostHeader.jsx
+++ src/cmps/PostHeader.jsx
@@ -1,20 +1,17 @@
 import { utilService } from "../services/util.service"
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
-import { useLocation } from 'react-router-dom'
 
-export function PostHeader({ post }) {
+export function PostHeader({ post, isPostDetailsPage}) {
     const timeSinceCreation = utilService.getTimeSinceCreation(post.createdAt)
-    const location = useLocation(/p/)
-
 
     return (
-        <div className={location ? "page-post-header" : "post-header"}>
+        <div className={isPostDetailsPage ? "page-post-header" : "post-header"}>
             <img src={post.by.imgUrl} className="profilePreviewImg"></img>
             <div className="post-header-main">
                 <button className="post-header-name">{post.by.fullName}</button>
                 <div className="dot">•</div>
-                {location ? null : <p className="passed-time">{timeSinceCreation}</p>}
-                {location ? null : <div className="dot">•</div>}
+                {isPostDetailsPage ? null : <p className="passed-time">{timeSinceCreation}</p>}
+                {isPostDetailsPage ? null : <div className="dot">•</div>}
                 <button className="follow-btn" >Follow</button>
             </div>
             <MoreHorizIcon className="more-horiz-icon" style={{ width: '20px', height: '20px' }} />
