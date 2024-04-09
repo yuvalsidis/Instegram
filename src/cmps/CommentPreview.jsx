51a48019913d322b9a219c405bfd772072810b8b
yuvalsidis <yuvalsidis@gmail.com>
2024-04-09 23:05:17 +0300

    Add CommentPreview elements and translate function

--- src/cmps/CommentPreview.jsx
+++ src/cmps/CommentPreview.jsx
@@ -1,8 +1,50 @@
-export function CommentPreview({comment}){
-  
+import { useState, useEffect } from "react";
+
+export function CommentPreview({ comment }) {
+    const [translatedText, setTranslatedText] = useState('');
+    const [isTranslatedToHebrew, setIsTranslatedToHebrew] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        async function translateText() {
+            setLoading(true); // Set loading state to true before fetching translation
+            try {
+                const targetLanguage = isTranslatedToHebrew ? 'iw' : 'en';
+                const response = await fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(comment.txt)}`);
+                const data = await response.json();
+                const translatedText = data[0][0][0];
+                setTranslatedText(translatedText);
+            } catch (error) {
+                console.error('Translation error:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        translateText();
+    }, [comment.txt, isTranslatedToHebrew]);
+
+
+    const toggleTranslation = () => {
+        setIsTranslatedToHebrew(!isTranslatedToHebrew);
+    };
+
+
     return (
         <>
-            <button>{comment.by.fullName}</button>
+            <div>
+                <img src={comment.by.imgUrl} className="profilePreviewImg"></img>
+            </div>
+            <p>
+                <span className="post-comment-fullname">{comment.by.fullName} </span>
+                {loading ? 'Loading...' : translatedText}
+            </p>
+            <div>
+                <p className="passed-time">{comment.createdAt}</p>
+                <button className="translate-btn" onClick={toggleTranslation}>
+                    {isTranslatedToHebrew ? "See orginal" : "See translation"}
+                </button>
+            </div>
         </>
     )
 }
