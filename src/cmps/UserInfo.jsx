7124698916d8c42f642573c05854a180ca8c7842
yuvalsidis <yuvalsidis@gmail.com>
2024-04-10 19:39:38 +0300

    Link files to ProfilePagejsx page

--- src/cmps/UserInfo.jsx
+++ src/cmps/UserInfo.jsx
@@ -1,10 +1,10 @@
+
 export function UserInfo(){
 
     return (
         <div className="user-info">
             <div className="user-info-img">Image</div>
             <div className="user-info-details">
-                
             </div>
             <h1>User Info</h1>
         </div>
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,11 +1,19 @@
+import { useState, useEffect } from "react"
+
 import { UserInfo } from "../cmps/UserInfo"
 import { UserContent } from "../cmps/UserContent"
+import { useSelector } from "react-redux"
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service"
+import { LOADING_DONE, LOADING_START } from "../store/system.reducer"
+import { utilService } from "../services/util.service"
+
 
 export function ProfilePage() {
+    
     return (
         <section className="profile-page">
             <UserInfo />
-            <UserContent/>
+            <UserContent />
         </section>
     )
 }
