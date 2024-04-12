f981be4714318b70dbaad5f6d8e64e79a22aff63
yuvalsidis <yuvalsidis@gmail.com>
2024-04-12 16:10:31 +0300

    Add basic layout in UserStats with conditional rendering

--- src/cmps/UserStats.jsx
+++ src/cmps/UserStats.jsx
@@ -1,14 +1,31 @@
 
-export function UserStats({ watchedUser, loggedInUser }){
-    return(
+export function UserStats({ watchedUser, loggedInUser }) {
+    return (
         <div className="user-stats">
             {watchedUser ?
                 <>
-                      
+                    <div className="post-stats-container">
+                        <p className="post-stat"> <span>{watchedUser.info.posts}</span> posts</p>
+                    </div>
+                    <div className="post-stats-container">
+                        <p className="followers-stat"><span>{watchedUser.info.followers.length}</span> followers</p>
+                    </div>
+                    <div className="post-stats-container">
+                        <p className="following-stat"><span>{watchedUser.info.following.length}</span> following</p>
+                    </div>
                 </>
                 :
                 <>
-                   
+                    <div className="post-stats-container">
+                        <p className="post-stat"><span>{loggedInUser.info.posts}</span> posts</p>
+                    </div>
+                    <div className="post-stats-container">
+                        <p className="followers-stat"><span>{loggedInUser.info.followers.length}</span> followers</p>
+                    </div>
+                    <div className="post-stats-container">
+                        <p className="following-stat"><span>{loggedInUser.info.following.length}</span> following</p>
+                    </div>
+
                 </>
             }
         </div>
