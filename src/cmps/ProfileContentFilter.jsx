ba527438ca1af317edc698f5c3559b60481d1969
yuvalsidis <yuvalsidis@gmail.com>
2024-04-13 12:58:14 +0300

    Add handleClickOnFilter and activeFilter in ProfileContentFilter and adjust rendering with classes

--- src/cmps/ProfileContentFilter.jsx
+++ src/cmps/ProfileContentFilter.jsx
@@ -1,30 +1,53 @@
 import GridOnIcon from '@mui/icons-material/GridOn';
+import { useState } from 'react';
 
 export function ProfileContentFilter({ watchedUser, loggedInUser }) {
+    const [activeFilter, setActiveFilter] = useState('posts')
+
+    function handleClickOnFilter(filterType) {
+        switch (filterType) {
+            case 'posts':
+                setActiveFilter('posts')
+                break
+            case 'saved':
+                setActiveFilter('saved')
+                break
+            default:
+                setActiveFilter('posts')
+                break
+        }
+
+    }
+
+    console.log('activeFilter', activeFilter)
 
     return (
         <div className="profile-content-filter">
             {watchedUser ?
                 <>
-                     <div className="posts-filter">
+                    <div className={`posts-filter ${activeFilter === 'posts' ? 'filterBorder' : ''}`} onClick={() => handleClickOnFilter("posts")}>
                         <div className='filter-container' >
                             <GridOnIcon className='profile-filter-icon' />
-                            <button className='profile-flter-btn'>POSTS</button>
+                            <button className='profile-filter-btn'>POSTS</button>
                         </div>
                     </div>
                 </>
                 :
                 <>
-                    <div className="posts-filter">
+                    <div className={`posts-filter ${activeFilter === 'posts' ? 'filterBorder' : ''}`} onClick={() => handleClickOnFilter("posts")}>
                         <div className='filter-container'>
-                            <GridOnIcon className='profile-filter-icon' />
-                            <button className='profile-flter-btn'>POSTS</button>
+                            <GridOnIcon className={`profile-filter-icon ${activeFilter === 'posts' ? 'selectedColor' : ''}`} />
+                            <button className={`profile-filter-btn ${activeFilter === 'posts' ? 'selectedColor' : ''}`}>POSTS</button>
                         </div>
                     </div>
-                    <div className="posts-save-filter">
+                    <div className={`posts-save-filter ${activeFilter === 'saved' ? 'filterBorder' : ''}`} onClick={() => handleClickOnFilter("saved")}>
                         <div className='filter-container'>
-                            <img className=' profile-filter-icon profile-bookmark-icon' src="../../public/icons/GrayBookmark.svg" alt="Gray Bookmark Icon" />
-                            <button className='profile-flter-btn'>SAVED</button>
+                            {activeFilter === 'saved' ?
+                                 <img className=' profile-filter-icon profile-bookmark-icon' src="../../public/icons/Bookmark.svg" alt="Bookmark Icon" />
+                                :
+                                <img className=' profile-filter-icon profile-bookmark-icon' src="../../public/icons/GrayBookmark.svg" alt="Gray Bookmark Icon" />
+                            }
+                            <button className={`profile-filter-btn ${activeFilter === 'saved' ? 'selectedColor' : ''}`}>SAVED</button>
                         </div>
                     </div>
                 </>
