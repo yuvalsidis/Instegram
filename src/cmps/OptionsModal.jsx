d74575466263a959471e0c70c320dd88de097946
yuvalsidis <yuvalsidis@gmail.com>
2024-04-15 00:15:23 +0300

    Add noscroll condition depend on specific page to OptionsModal

--- src/cmps/OptionsModal.jsx
+++ src/cmps/OptionsModal.jsx
@@ -1,8 +1,24 @@
 
-return function OptionModal() {
+import { useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+
+export function OptionsModal({setIsOptionsModalOpen}) {
+    const locationPathname = useLocation().pathname
+
+
+    useEffect(() => {
+        if(locationPathname.includes('profile') || locationPathname.includes('explore')) return
+        document.body.classList.add("no-scroll")
+        return () => {
+            document.body.classList.remove("no-scroll");
+        }
+    }, [])
+
     return (
         <section className="options-modal">
-
+        {locationPathname.includes('profile') && (
+            <button className='delete-btn'>Delete</button>
+        )}
         </section>
     )
 }
