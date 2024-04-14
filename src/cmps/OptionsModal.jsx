d4b8cdd5c19ec09da5e356a4d31a862e4e8ba248
yuvalsidis <yuvalsidis@gmail.com>
2024-04-15 00:31:24 +0300

    Adjust layout for options modal and buttons at all

--- src/cmps/OptionsModal.jsx
+++ src/cmps/OptionsModal.jsx
@@ -2,23 +2,30 @@
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 
-export function OptionsModal({setIsOptionsModalOpen}) {
+export function OptionsModal({ setIsOptionsModalOpen }) {
     const locationPathname = useLocation().pathname
 
 
     useEffect(() => {
-        if(locationPathname.includes('profile') || locationPathname.includes('explore')) return
+        if (locationPathname.includes('profile') || locationPathname.includes('explore')) return
         document.body.classList.add("no-scroll")
         return () => {
             document.body.classList.remove("no-scroll");
         }
     }, [])
 
+    function handleClickOnCancelBtn(){
+        setIsOptionsModalOpen(false)
+    }
+
     return (
         <section className="options-modal">
-        {locationPathname.includes('profile') && (
-            <button className='delete-btn'>Delete</button>
-        )}
+            {locationPathname.includes('profile') && (
+                <>
+                    <button className='delete-btn'>Delete</button>
+                    <button className='cancel-btn' onClick={handleClickOnCancelBtn}>Cancel</button>
+                </>
+            )}
         </section>
     )
 }
