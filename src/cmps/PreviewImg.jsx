ebe0f6205ae2f678ab8797658e75e136a2467707
alon.sitman <alonsitman@gmail.com>
2024-04-15 14:07:23 +0300

    Add upload multiple images in a img-preview carousel

--- src/cmps/PreviewImg.jsx
+++ src/cmps/PreviewImg.jsx
@@ -15,18 +15,18 @@ export function PreviewImg({ uploadedImages, previewImgUrl, onUploadMore, onUplo
     }
 
     
-    return (<>
-        {(uploadedImages.length > 1 && uploadedImages.length - 1 > currentImageIndex &&
-            <button ref={nextImageButtonRef} onClick={handleNextImg}>right</button>)}
-        
+    return (
+    <div className="img-preview">
         <img src={previewImgUrl} style={{ width: '100%' }} />
-        <button className="upload-more-btn" onClick={onUploadMore}>More photos</button>
-        {/* <button className="upload-image-btn" onClick={() => document.getElementById('imgUpload').click()}>
-              More photos
-              </button> */}
-        <ImgUploader onUploaded={onUploaded} />
-        {(uploadedImages.length > 1 && currentImageIndex > 0 &&  
-            <button ref={prevImageButtonRef} onClick={handlePrevImg}>left</button>)}
         
-        </>)
+        <div className="rotate-btns">
+            {(uploadedImages.length > 1 && currentImageIndex > 0 &&  
+                <button className="rotate-left" ref={prevImageButtonRef} onClick={handlePrevImg}>left</button>)}
+            {(uploadedImages.length > 1 && uploadedImages.length - 1 > currentImageIndex &&
+                <button className="rotate-right" ref={nextImageButtonRef} onClick={handleNextImg}>right</button>)}
+        </div>
+        
+        <button className="upload-more-btn" onClick={onUploadMore}>More photos</button>
+        <ImgUploader onUploaded={onUploaded} />
+    </div>)
 }
