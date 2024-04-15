fc299919e450ab7bb7e1808b6eaa1df881cbdc59
yuvalsidis <yuvalsidis@gmail.com>
2024-04-15 21:12:11 +0300

    Adjust css syle and layout in FollowerPreview

--- src/cmps/FollowerPreview.jsx
+++ src/cmps/FollowerPreview.jsx
@@ -6,11 +6,11 @@ export function FollowerPreview({user, isWatchedUser}) {
                   <img className="profilePreviewImg user-img" src={user.imgUrl} alt="profile-img"></img>
             </div>
             <div className="user-names">
-                 <p>{user.username}</p>
-                 <p>{user.firstname} {user.lastname}</p>
+                 <p className="username-p">{user.username}</p>
+                 <p className="firstname-secondname-p">{user.firstname} {user.lastname}</p>
             </div>
-            <div className="follow-btn-action">
-                <button>Following</button>
+            <div className="follow-btn-container">
+                <button className="following-btn">Following</button>
             </div>
         </>
     )
