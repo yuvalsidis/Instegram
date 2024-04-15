a6faa620f4023470c17ead3e63f14104026625a3
yuvalsidis <yuvalsidis@gmail.com>
2024-04-15 20:39:35 +0300

    Add FollowerList cmp

---
+++ src/cmps/FollowerPreview.jsx
@@ -0,0 +1,8 @@
+
+export function FollowerPreview(){
+    return (
+        <>
+        <h1>hi</h1>
+        </>
+    )
+}
--- src/cmps/FollowersList.jsx
+++ src/cmps/FollowersList.jsx
@@ -1,14 +1,15 @@
+import { FollowerPreview } from "./FollowerPreview"
 
 export function FollowerList({fullUser, isWatchedUser}){
 
     if(!fullUser) return <div>Loading</div>
-    
+
      return (
         <div className="follower-list">
              {
                 fullUser.info.following.map(user => (
                     <div key={`follower-${user.id}`} className="follower-preview">
-                              <h1>Hi</h1>
+                              <FollowerPreview/>
                     </div>
                 ))
              }
