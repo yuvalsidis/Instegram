832ca0236ba0c1208a2b07b1d5570d8cb743135e
yuvalsidis <yuvalsidis@gmail.com>
2024-04-16 13:09:16 +0300

    Adjust condition rendering in RemoveFollowerModal

--- src/cmps/RemoveFollowerModal.jsx
+++ src/cmps/RemoveFollowerModal.jsx
@@ -1,19 +1,41 @@
 
-export function RemoveFollowerModal({isWatchedUser ,user}){
+export function RemoveFollowerModal({ isWatchedUser, user }) {
 
     return (
-        <section className="remove-follower-modal">
-             <div className="user-img-container">
-                <img className="profilePreviewImg user-img" src={user.imgUrl} alt="profile-img"></img>
-            </div>
-            <div className="remove-follower-modal-desc">
-                <p className="question">Remove follower?</p>
-                <p className="details">{`instegram won't tell ${user.username} they were removed from your followers`}</p>
-            </div>
-            <div className="remove-follower-modal-btns">
-                <button className="unfollow-n-remove-btn">Remove</button>
-                <button className="cancel-btn">Cancel</button>
-            </div>
-        </section>
+        <>
+            {isWatchedUser ?
+                <>
+                    <section className="remove-follower-modal remove-follower-modal-adjustment">
+                        <div className="user-img-container">
+                            <img className="profilePreviewImg user-img" src={user.imgUrl} alt="profile-img"></img>
+                        </div>
+                        <div className="remove-follower-modal-desc">
+                            <p className="details">{`If you change your mind, you'll have to request to follow ${user.username} again.`}</p>
+                        </div>
+                        <div className="remove-follower-modal-btns">
+                            <button className="unfollow-n-remove-btn">Unfollow</button>
+                            <button className="cancel-btn">Cancel</button>
+                        </div>
+                    </section>
+                </>
+                :
+                <>
+                    <section className="remove-follower-modal">
+                        <div className="user-img-container">
+                            <img className="profilePreviewImg user-img" src={user.imgUrl} alt="profile-img"></img>
+                        </div>
+                        <div className="remove-follower-modal-desc">
+                            <p className="question">Remove follower?</p>
+                            <p className="details">{`instegram won't tell ${user.username} they were removed from your followers`}</p>
+                        </div>
+                        <div className="remove-follower-modal-btns">
+                            <button className="unfollow-n-remove-btn">Remove</button>
+                            <button className="cancel-btn">Cancel</button>
+                        </div>
+                    </section>
+                </>
+
+            }
+        </>
     )
 }
