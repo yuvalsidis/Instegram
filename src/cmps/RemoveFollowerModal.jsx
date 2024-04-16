748cf1b1ee92ee23ca1fe7c31e065418acdb9bd5
yuvalsidis <yuvalsidis@gmail.com>
2024-04-16 12:47:38 +0300

    Add removefollowermodaldesac scss style and layout question and details inside as well

--- src/cmps/RemoveFollowerModal.jsx
+++ src/cmps/RemoveFollowerModal.jsx
@@ -6,6 +6,10 @@ export function RemoveFollowerModal({isWatchedUser ,user}){
              <div className="user-img-container">
                 <img className="profilePreviewImg user-img" src={user.imgUrl} alt="profile-img"></img>
             </div>
+            <div className="remove-follower-modal-desc">
+                <p className="question">Remove follower?</p>
+                <p className="details">{`instegram won't tell ${user.username} they were removed from your followers`}</p>
+            </div>
         </section>
     )
 }
