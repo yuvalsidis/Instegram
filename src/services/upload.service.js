067e65a20e6d923327fe734d2ef9e2c3c1b9645f
yuvalsidis <yuvalsidis@gmail.com>
2024-04-17 22:12:52 +0300

    uploadservice add key

--- src/services/upload.service.js
+++ src/services/upload.service.js
@@ -2,7 +2,7 @@ export const uploadService = {
   uploadImg
 }
 async function uploadImg(ev) {
-  const CLOUD_NAME = "dcwibf9o5"
+  const CLOUD_NAME = "dldbqjl5h"
   const UPLOAD_PRESET = "vt0iqgff"
   const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
 
