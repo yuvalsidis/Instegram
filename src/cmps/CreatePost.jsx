b23e8d972596d0520474ff6b05dccc064458d910
yuvalsidis <yuvalsidis@gmail.com>
2024-04-19 17:13:18 +0300

    Add and link CreatePost jsx and scss

---
+++ src/cmps/CreatePost.jsx
@@ -0,0 +1,7 @@
+export function CreatePost(){
+   return (
+    <div className="create-post">
+        
+    </div>
+   )   
+}
--- src/cmps/CreatePostContainer.jsx
+++ src/cmps/CreatePostContainer.jsx
@@ -1,6 +1,9 @@
+import { CreatePost } from "./CreatePost"
 
 export function CreatePostContainer(){
     return (
-        <section className="create-post-container">CreatePost</section>
+        <section className="create-post-container">
+             <CreatePost/>
+        </section>
     )
 }
