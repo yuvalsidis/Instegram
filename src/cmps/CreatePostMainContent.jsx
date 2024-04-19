065b6455b19d7dfc8ce4b09fa6aaa0d32266a530
yuvalsidis <yuvalsidis@gmail.com>
2024-04-19 18:20:50 +0300

    Add basic layout and scss to CreatePostMain and CreatePostMainContent and add CreatePost svg

--- src/cmps/CreatePostMainContent.jsx
+++ src/cmps/CreatePostMainContent.jsx
@@ -1,8 +1,16 @@
 
-export function CreatePostMainContent(){
+export function CreatePostMainContent() {
     return (
         <div className="create-post-main-content">
-             <div>main content</div>
+            <div>
+                <img className="create-post-icon" src="/public/icons/CreatePost.svg" alt="Create Post Icon" />
+            </div>
+            <div className="drag-photos-p">
+                <p>Drag photos and videos here</p>
+            </div>
+            <div>
+                <button className="select-from-computer-btn">Select from computer</button>
+            </div>
         </div>
     )
 }
