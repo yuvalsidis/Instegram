e9436f17e696ab442180aa16be2bd03bb3239799
yuvalsidis <yuvalsidis@gmail.com>
2024-04-19 23:10:59 +0300

    Adjust layout of createpostmain

--- src/cmps/CreatePostMainFilter.jsx
+++ src/cmps/CreatePostMainFilter.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react"
 
 export function CreatePostMainFilter() {
+    const [activeFilter, setActiveFilter] = useState(null);
+
 
     // will be filters and handle on filter and then adjust the filter on that class create-post-filter
-
+     
     return (
         <div className="create-post-main-filter">
             <div className="create-post-main-filter-title">Filters</div>
