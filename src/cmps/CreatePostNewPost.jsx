8bee42fe0c6d10803480a0b599119a06186e8a11
yuvalsidis <yuvalsidis@gmail.com>
2024-04-20 00:45:53 +0300

    Add basic layout of CreatePostNewPost

--- src/cmps/CreatePostNewPost.jsx
+++ src/cmps/CreatePostNewPost.jsx
@@ -1,13 +1,20 @@
 import { Store } from "@mui/icons-material"
 import { useSelector } from "react-redux"
- 
+import { postService } from "../services/post.service.local"
+import { useState } from "react"
+
 export function CreatePostNewPost(){
   const loggedInUser = useSelector(storeState => storeState.userModule.user)
+  const [caption, setCaption] = useState(postService.getEmptyPost())
 
     return (
       <div className="create-post-new-post"> 
             <div className="profile-info-container">
-              <img src={loggedInUser.imgUrl}></img>
+              <img className="profilePreviewImg" src={loggedInUser.imgUrl}></img>
+              <p>{loggedInUser.username}</p>
+            </div>
+            <div>
+                    
             </div>
       </div>
     )
