eff644e35b6d165f6e9b9f8a9eeb447e5648a3f8
yuvalsidis <yuvalsidis@gmail.com>
2024-04-19 22:28:32 +0300

    Add filterImg for create posts

--- src/cmps/CreatePostMainFilter.jsx
+++ src/cmps/CreatePostMainFilter.jsx
@@ -2,10 +2,11 @@
 export function CreatePostMainFilter(){
 
     // will be filters and handle on filter and then adjust the filter on that class create-post-filter
-    
+
     return (
         <div className="create-post-main-filter">
-               <div>filter</div>
+               <div className="create-post-main-filter-title">Filters</div>
+               <img src=""></img>
         </div>
     )
 }
