e5a6e90fa0efae77ebf1b2452d0e3b6d22131e5b
yuvalsidis <yuvalsidis@gmail.com>
2024-04-20 17:22:44 +0300

    Add POST CREATED state managment in system reducer

--- src/store/system.reducer.js
+++ src/store/system.reducer.js
@@ -1,16 +1,23 @@
 export const LOADING_START = 'LOADING_START'
 export const LOADING_DONE = 'LOADING_DONE'
+export const POST_CREATED = 'POST_CREATED'
+export const POST_NOT_CREATED ='POST_NOT_CREATED'
 
 const initialState = {
-  isLoading: false
+  isLoading: false,
+  isPostCreated: false
 };
 
-export function systemReducer (state = initialState, action = {}) {
+export function systemReducer(state = initialState, action = {}) {
   switch (action.type) {
     case LOADING_START:
       return { ...state, isLoading: true }
     case LOADING_DONE:
       return { ...state, isLoading: false }
+    case POST_CREATED:
+      return { ...state, isPostCreated: true }
+    case POST_NOT_CREATED:
+      return { ...state, isPostCreated: false }
     default: return state
   }
 }
